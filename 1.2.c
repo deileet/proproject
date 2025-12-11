bbc19bc8fe73db3bf4feceb8d4cfe897174fc244
Сафонов Денис ТКИ-141 <deniscreative2@mail.ru>
2025-12-11 23:37:59 +0300

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
- 
+#include <float.h>
 /**
  * @brief считывает значение, введенное с клавиатуры с проверкой ввода
  * @return считанное значение
@@ -18,10 +18,10 @@ void checkValue(const double value);
  
 /**
  * @brief Программа вычисляет площадь круга, если известна длина
- * @param dlina Параметр длины круга
+ * @param length Параметр длины круга
  * @return Возвращает вычисленную площадь круга  
  */
-double getSquare(const double dlina);
+double getSquare(const double length);
  
  
 /**
@@ -29,10 +29,10 @@ double getSquare(const double dlina);
  * @return возвращает 0, если программма выполнена корректно
  */
 int main(void){
-    printf("Введите число:\n");
-    double dlina = getValue();
-    checkValue(dlina);
-    printf("square = %.2lf\n", getSquare(dlina));
+    printf("Enter value:\n");
+    double length = getValue();
+    checkValue(length);
+    printf("square = %.2lf\n", getSquare(length));
     return 0;
 }
  
@@ -40,21 +40,21 @@ double getValue(void){
     double value = 0;
     if (!scanf("%lf", &value))
     {
-        printf("Error\n");
-        abort();
+        fprintf(stderr, "Error\n");
+        exit(1);
     }
     return value;
 }
  
 void checkValue(const double value){
-    if (value <= 0)
+    if (!(value >= DBL_EPSILON))
     {
-        printf("Value have to be possitive\n");
-        abort();
+        fprintf(stderr, "Value have to be possitive\n");
+        exit(1);
     }
 }
  
-double getSquare(const double dlina){
-    double rad = dlina / (2 * M_PI);
-    return rad * rad * M_PI;
+double getSquare(const double length){
+    double rad = length / (2 * (M_PI));
+    return rad * rad * (M_PI);
 }
