7d77e8f31f64102803976d76799c2029c5ef8ee1
Сафонов Денис ТКИ-141 <deniscreative2@mail.ru>
2025-12-19 00:22:44 +0300

    Update 3.1.c

--- 3.1.c
+++ 3.1.c
@@ -23,6 +23,13 @@ void checkStep(const double step);
  */
 double getY(const double x);
 
+
+/**
+ * @brief проверят, больше ли старт функции или нет
+ * @return возвращает ошибку, если старт больше чем конец, иначе ничего не возвращает
+ */
+void checkStartEnd(const double start, const double end);
+
 /**
  * @brief Точка входа в программу
  * @return возвращает 0, если программма выполнена корректно
@@ -34,10 +41,11 @@ int main(void)
 	const double start = getValue();
 	printf("Enter the final value: ");
 	const double end = getValue();
+	checkStartEnd(start, end);
 	printf("Enter step: ");
 	const double step = getValue();
 	checkStep(step);
-	for (double x = start; x < end + DBL_EPSILON; x = x + step)
+	for (double x = start; x <= end + DBL_EPSILON; x = x + step)
 	{
         printf("x = %.2lf, y = %.4lf\n", x, getY(x));
 	}
@@ -66,6 +74,16 @@ void checkStep(const double step)
 	if (step <= DBL_EPSILON)
 	{
 		fprintf(stderr, "Error!\n");
+		exit(1);
+	}	
+}
+
+
+void checkStartEnd(const double start, const double end)
+{
+	if (start > end + DBL_EPSILON)
+	{
+		fprintf(stderr, "Error!");
 		exit(1);
 	}
 }
